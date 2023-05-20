979e5ee29dc1dbdedb4fb5988ab5c2a05cd77c5e
HARISHMECH-2003 <128779068+HARISHMECH-2003@users.noreply.github.com>
2023-05-20 08:18:32 +0100

    Add files via upload

---
+++ STRING2/double_char.py
@@ -0,0 +1,6 @@
+def double_char(str):
+  result=''
+  for char in str:
+    char1=char*2
+    result+=char1
+  return result
---
+++ STRING2/end_other.py
@@ -0,0 +1,3 @@
+def end_other(a, b):
+   return a.lower().endswith(b.lower()) or b.lower().endswith(a.lower())
+ 
